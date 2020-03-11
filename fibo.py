606878359c08fa74d1ad0447e73ac6b88306e6b9
Weverson Nascimento <weverson@ufpa.br>
2020-03-11 17:08:16 -0300

    códigos para exemplificação de módulos

---
+++ fibo.py
@@ -0,0 +1,16 @@
+# Módulo de números de Fibonacci
+
+def fib(n): # escreve a sequência de Fibonacci até n
+    a, b = 0, 1
+    while a < n:
+        print(a, end=' ')
+        a, b = b, a + b
+    print()
+
+def fib2(n): # retorna a sequência de Fibonacci até n
+    result = []
+    a, b = 0, 1
+    while a < n:
+        result.append(a)
+        a, b = b, a + b
+    return result
